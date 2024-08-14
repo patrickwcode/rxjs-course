ea546db797508e522b925c0ab32a50ee59b9ab4c
Patrick Wauschek <patrick@patrickwcode.com>
2024-08-14 13:04:28 -0500

    Creates numbers observable and renders data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'rxjs-course';
+  data = 0;
+  constructor() {
+    const numbers$ = from([1, 2, 3, 4, 5]);
+
+    numbers$.subscribe((data) => {
+      console.log('subscriber', data);
+      this.data = data;
+    });
+  }
 }
